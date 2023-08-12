2e62436ca8c309e82eb527ba5734da0a6618d66f
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2023-08-13 02:40:11 +0600

    styled and give more logic add readme.md

---
+++ README.md
@@ -0,0 +1,75 @@
+# Typing Speed Web Application
+
+Welcome to the Typing Speed Web Application! This web app is designed to help users improve their typing speed and accuracy. Users can test their typing skills by typing a given text as quickly and accurately as possible. The application provides real-time feedback on typing speed, accuracy, and errors.
+
+![Typing Speed Web App Screenshot](screenshot.png)
+
+## Features
+
+- Test your typing speed and accuracy with a variety of texts.
+- Real-time feedback on your typing speed and errors.
+- Simple and intuitive user interface.
+- Built using HTML, Vanilla JavaScript, and Tailwind CSS.
+
+## Usage
+
+Visit the live Typing Speed Web Application: [https://Md-Nur.github.io/typing-test](https://Md-Nur.github.io/typing-test)
+
+1. Open the provided link in your preferred web browser.
+
+2. Choose a text to type from the available options.
+
+3. Start typing the displayed text in the provided input area.
+
+4. Your typing speed, accuracy, and errors will be displayed in real-time as you type.
+
+## Demo
+
+You can see the live demo of the Typing Speed Web Application [here](https://Md-Nur.github.io/typing-test).
+
+## Logical code
+
+```javascript
+textElement.addEventListener("keydown", (e) => {
+  if (initTime === 0) {
+    initTime = new Date();
+  }
+  timing = setInterval(() => {
+    time = Math.floor((new Date() - initTime) / 1000); // calculate the time in seconds
+    if (time === 60) {
+      // Chceck if the time is over
+      endGame();
+      timeElement.innerHTML = `Time Up!!`; // show the remaining time to the user in seconds
+      return;
+    } else if (time < 60) {
+      timeElement.innerHTML = `Time remains ${60 - time} second`; // show the remaining time to the user in seconds
+    }
+  }, 1000);
+  if (e.key === " " || e.key === "Enter") {
+    if (textElement.value.trim() === wordElement.innerHTML) {
+      // check if the word is correct
+      correctWords++;
+    } else {
+      incorrectWords++; // if the word is incorrect
+    }
+    const newWord =
+      wordList[Math.floor(Math.random() * wordList.length)].toLowerCase();
+    wordElement.innerHTML = newWord;
+    textElement.value = "";
+  }
+});
+```
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+Happy typing!
+
+For more projects, visit [Code & Cognition](https://code-n-cognition.vercel.app/).
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// all word list that used to generate random word
 const wordList = [
   "Hello",
   "World",
@@ -109,46 +110,62 @@ const wordList = [
   "Dog",
 ];
 
+// Capture all the elements
 const wordElement = document.getElementById("word");
 const textElement = document.getElementById("inputText");
 const timeElement = document.getElementById("time");
 const scoreElement = document.getElementById("score");
 
-// Init word
-wordElement.innerHTML =
-  wordList[Math.floor(Math.random() * wordList.length)].toLowerCase();
+// Initialize the variables
 let initTime = 0;
 let time;
+let timing;
 let correctWords = 0;
 let incorrectWords = 0;
-textElement.addEventListener("input", (e) => {
+
+// Set the first word to start the test
+wordElement.innerHTML =
+  wordList[Math.floor(Math.random() * wordList.length)].toLowerCase();
+
+// This function is called when the time is over and show the result to the user
+const endGame = () => {
+  alert("Game Over");
+  textElement.value = "";
+
+  //show the result to the user
+  scoreElement.innerHTML = `Your typing speed is ${correctWords} WPM <br> Your accuracy is ${Math.floor(
+    (correctWords / (correctWords + incorrectWords)) * 100
+  )}% <br> correct words: ${correctWords} <br> incorrect words: ${incorrectWords}`;
+  clearInterval(timing);
+};
+
+
+// This function count the correct and incorrect words
+textElement.addEventListener("keydown", (e) => {
   if (initTime === 0) {
     initTime = new Date();
   }
-  const timing = setInterval(() => {
-    time = Math.floor((new Date() - initTime) / 1000);
-    timeElement.innerHTML = `Time remains ${60 - time} second`;
+  timing = setInterval(() => {
+    time = Math.floor((new Date() - initTime) / 1000); // calculate the time in seconds
+    if (time === 60) {
+      // Chceck if the time is over
+      endGame();
+      timeElement.innerHTML = `Time Up!!`; // show the remaining time to the user in seconds
+      return;
+    } else if (time < 60) {
+      timeElement.innerHTML = `Time remains ${60 - time} second`; // show the remaining time to the user in seconds
+    }
   }, 1000);
-
-  if (time >= 60) {
-    alert("Game Over");
-    e.target.setAttribute("disabled", "disabled");
-    e.target.value = "";
-    scoreElement.innerHTML = `Your typing speed is ${correctWords} WPM`;
-    clearInterval(timing);
-    return;
-  }
-});
-
-textElement.addEventListener("keydown", (e) => {
   if (e.key === " " || e.key === "Enter") {
     if (textElement.value.trim() === wordElement.innerHTML) {
+      // check if the word is correct
       correctWords++;
     } else {
-      incorrectWords++;
+      incorrectWords++; // if the word is incorrect
     }
-    wordElement.innerHTML =
+    const newWord =
       wordList[Math.floor(Math.random() * wordList.length)].toLowerCase();
+    wordElement.innerHTML = newWord;
     textElement.value = "";
   }
 });
